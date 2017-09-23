a342886d471b1311dc6896f624f18f121e4b75e8
Rob Berry <rob@luvhate.us>
2017-09-23 19:41:10 +0100

    Convert POST data to correct json format if possible

--- src/Mailsift/Server.hs
+++ src/Mailsift/Server.hs
@@ -1,35 +1,62 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE GADTs #-}
 
 module Mailsift.Server where
 
-import Control.Monad.IO.Class (liftIO)
-import           Control.Monad.Logger    (LoggingT, runStdoutLoggingT)
-import           Database.Persist.Sqlite (SqlBackend, SqlPersistT,
-                                          createSqlitePool, runSqlConn)
-import           Mailsift.Config         (Environment (Development),
-                                          lookupSetting, setLogger)
-import           Mailsift.Entities       ()
-import           Network.Wai             (Request)
 
-import Network.Wai.Parse (parseRequestBody, lbsBackEnd)
-import           Web.Spock               (HasSpock, SpockAction, SpockConn,
-                                          SpockM, body, get, json, jsonBody', middleware,
-                                          post, request, root, runQuery,
-                                          runSpock, spock, text)
-import           Web.Spock.Config        (PoolOrConn (PCPool), defaultSpockCfg)
+import           Control.Monad.IO.Class    (liftIO)
+import           Control.Monad.Logger      (LoggingT, runStdoutLoggingT)
+import           Database.Persist.Sqlite   (SqlBackend, SqlPersistT,
+                                            createSqlitePool, runSqlConn)
+import           Mailsift.Config           (Environment (Development),
+                                            lookupSetting, setLogger)
+import           Mailsift.Entities         ()
+import           Network.HTTP.Types.Status (status422)
+import           Network.Wai               (Request)
 
-import           Data.Aeson              (toJSON, Value (Object), Value, (.=))
+import           Network.Wai.Parse         (lbsBackEnd, parseRequestBody)
+import           Web.Spock                 (HasSpock, SpockAction, SpockConn,
+                                            SpockM, body, get, json, jsonBody,
+                                            middleware, post, request, root,
+                                            runQuery, runSpock, setStatus,
+                                            spock, text)
+import           Web.Spock.Config          (PoolOrConn (PCPool),
+                                            defaultSpockCfg)
 
-import           GHC.Exts                (fromList)
+import           Data.Aeson                (FromJSON, KeyValue, ToJSON,
+                                            Value (Object), Value, eitherDecode, encode,
+                                            parseJSON, toJSON, (.:), (.=))
+import           Data.Aeson.Types          (camelTo2, defaultOptions,
+                                            fieldLabelModifier, genericToJSON,
+                                            withObject)
 
-import qualified Data.ByteString         as B
-import           Data.Text.Encoding      (decodeUtf8)
-import Data.Text (Text)
+import           GHC.Exts                  (fromList)
+
+import qualified Data.ByteString           as B
+import qualified Data.Text as T
+import           Data.Text                 (Text)
+import           Data.Text.Encoding        (decodeUtf8)
+import           GHC.Generics              (Generic)
 
 type Api = SpockM SqlBackend () () ()
 
 type ApiAction a = SpockAction SqlBackend () () a
+
+
+data Email = Email
+  { emailTo :: Text
+  , emailFrom :: Text
+  , emailSubject :: Text
+  } deriving (Show, Generic)
+
+
+instance FromJSON Email where
+  parseJSON = withObject "Email" $ \o -> Email <$> o .: "to" <*> o .: "from" <*> o .: "subject"
+
+
+instance ToJSON Email where
+  toJSON = genericToJSON defaultOptions { fieldLabelModifier = drop 6 . camelTo2 '_' }
 
 
 run :: IO ()
@@ -41,14 +68,26 @@ run = do
   spockCfg <- defaultSpockCfg () (PCPool pool) ()
   runSpock port (spock spockCfg $ middleware logger >> app)
 
+
 app :: Api
 app = do
-  get root $ do
-    text "hiya"
+  get root $ text "hiya"
   post "parse" $ do
     req <- request
     (params, _) <- liftIO $ parseRequestBody lbsBackEnd req
-    json . Object . fromList $ (map (\(a, b) -> (decodeUtf8 a .= decodeUtf8 b)) params)
+    let dynJson = Object . fromList $ map expand params
+    let dynValue = encode dynJson
+    let xo = eitherDecode dynValue :: Either String Email
+    email <- case xo of
+      Left err -> do
+        setStatus status422
+        json (Object $ fromList ["error" .= T.pack err])
+      Right val -> return val
+    json email
+
+
+expand :: (KeyValue kv) => (B.ByteString, B.ByteString) -> kv
+expand (key, value) = decodeUtf8 key .= decodeUtf8 value
 
 
 runSQL
